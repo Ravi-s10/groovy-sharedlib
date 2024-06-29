3fb46fdd8ead8b1bcd1e707da567ab1e27d7895a
Ravi Sharma (non-empl, Ratings - Tech) <Ravi.Sharma-non-empl@moodys.com>
2024-06-29 11:49:24 +0530

    added code in runDocker groovy script

--- vars/runDocker.groovy
+++ vars/runDocker.groovy
@@ -1,1 +1,28 @@
-def call
+def call(String imageName, String containerName, String command='') {
+    pipeline {
+        agent any
+        stages {
+            stage('Run Docker Container') {
+                steps {
+                    script {
+                        // pull docker image from dockerhub
+                        sh "docker pull ${imageName}"
+
+                        // run docker container
+                        if (command) {
+                            sh "docker run --name ${containerName} ${imageName} ${command}"
+                        }
+                        else {
+                            sh "docker run --name ${containerName} ${imageName}"
+                        }
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                sh "docker rm -f ${containerName}"
+            }
+        }
+    }
+}
